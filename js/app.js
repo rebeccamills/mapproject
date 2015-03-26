84236a81658ac8bfe9daf3f46025bf9c77f52ca0
Rebecca Mills <vena.cava@gmail.com>
2015-03-26 15:48:07 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoicmViZWNjYW1pbGxzIiwiYSI6InRMeGMwSzgifQ.-_AkRm4mAT872PmddNfddg';
+var map = L.mapbox.map('map', 'rebeccamills.lifog24n')
+    .setView([38.922, -77.011], 16);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+// var layer = L.mapbox.tileLayer('layer')
+
+  
